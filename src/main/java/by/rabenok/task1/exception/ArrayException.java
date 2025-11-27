6239966ef69ed95db1a8814a837e287479258127
Ludmila_Rabenok <mila_k.lf@mail.ru>
2025-11-27 17:45:52 +0300

    commit 4

--- src/main/java/by/rabenok/task1/exception/ArrayException.java
+++ src/main/java/by/rabenok/task1/exception/ArrayException.java
@@ -4,5 +4,4 @@ public class ArrayException extends Exception {
   public ArrayException(String message) {
     super(message);
   }
-
 }
--- src/main/java/by/rabenok/task1/parser/ArrayParser.java
+++ src/main/java/by/rabenok/task1/parser/ArrayParser.java
@@ -1,6 +1,5 @@
 package by.rabenok.task1.parser;
 
 public interface ArrayParser {
-
   int[] parse(String line);
 }
